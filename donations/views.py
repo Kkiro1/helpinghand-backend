6e361fa135063b63b05a11acc302f74e969e2978
Nardeen <nardeenkhalaf9@gmail.com>
2025-12-11 23:44:03 +0200

    Views(logic)

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Donation
+from .serializers import DonationSerializer
 
-# Create your views here.
+
+class DonationListCreateView(generics.ListCreateAPIView):
+
+    serializer_class = DonationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only show donations of the logged-in user
+        return Donation.objects.filter(donor=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        # Save with logged-in user as donor
+        donation = serializer.save(donor=self.request.user)
+
+        # Simple update of campaign current_amount
+        campaign = donation.campaign
+        campaign.current_amount += donation.amount
+        campaign.save()
