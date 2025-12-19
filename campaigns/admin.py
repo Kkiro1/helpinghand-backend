b807215792761681aea585163d2ce84a30b85f06
Kerollos Emad <kerollose543@gmail.com>
2025-12-19 14:21:37 +0200

    missing file

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Campaign
 
-# Register your models here.
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "goal_amount", "current_amount", "is_active", "start_date", "end_date")
+    list_filter = ("is_active", "category")
+    search_fields = ("title", "description", "owner__username")
